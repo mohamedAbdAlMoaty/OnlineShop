7f836d75b78da0bb57ec5bda55f5b7e4f90ced5d
mohamed <test.test@test>
2019-01-31 03:08:18 +0200

    second edit

--- app/src/main/java/com/example/hp/onlineshop/Activities/HomeActivity.java
+++ app/src/main/java/com/example/hp/onlineshop/Activities/HomeActivity.java
@@ -20,7 +20,6 @@ public class HomeActivity extends BaseActivity {
     BottomNavigationView bottomNavigationView;
     Toolbar toolbar;
     TextView title;
-    String test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
